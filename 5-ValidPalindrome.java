575dc56c4c7099770f6d117cd02a1057a53bb27f
zachuri <punsalang.zachary@gmail.com>
2022-11-29 05:19:36 -0800

    5-ValidPalindrom: added plans

--- 5-ValidPalindrome.java
+++ 5-ValidPalindrome.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean isPalindrome(String s) {
+    // 1. Check if string is a letter or number Character.is()...
+    // 2. Lowercase all of them
+    // 3. create index count for start and end of array
+    // 4. check if each index is the same else false;
 		String temp = "";        
 
 		for (char c : s.toCharArray()) {
