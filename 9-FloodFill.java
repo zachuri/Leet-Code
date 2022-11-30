0e3e5756dfcf80c59ff36e2d454dcef58eea6ee9
zachuri <punsalang.zachary@gmail.com>
2022-11-30 04:44:23 -0800

    9-FloodFill: updated and added plans

--- 9-FloodFill.java
+++ 9-FloodFill.java
@@ -1,4 +1,12 @@
 class Solution {
+
+  // 1. Create a dfs
+  // 2. get current color
+  // 3. see if it doesn't equal updated color -> call dfs() 
+  // 4. Check the out of bounds
+  // 5. Check if current row is updated 
+  // 6. Recrusive call made updates per row and col 
+
   public int[][] floodFill(int[][] image, int sr, int sc, int color) {
     //Check if current color is equal with the current row/col
     int currColor = image[sr][sc];
@@ -19,13 +27,15 @@ class Solution {
     if (r >= image.length) { return; }
     if (c >=image[0].length) { return; }
 
-    // Check current color
+    // Check current row and color is already updated
     if (image[r][c] != currColor) { return; }
 
+    //Change current row/col to updated color
     image[r][c] = color;
     dfs(image, r-1, c, currColor, color);
     dfs(image, r, c-1, currColor, color);
     dfs(image, r+1, c, currColor, color);
     dfs(image, r, c+1, currColor, color);
   }
+}
 }
