1816bfdcf03b1378f1f41c360a0f9ff5446366b5
zachuri <punsalang.zachary@gmail.com>
2022-11-30 04:36:14 -0800

    9-FloodFill: added plan/ comments

--- 9-FloodFill.java
+++ 9-FloodFill.java
@@ -1,34 +1,31 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-		// Check if current color is equal with the current row/column
-        int currColor = image[sr][sc];
-		if (currColor != color) {
-            dfs(image, sr, sc, currColor, color);
-		}
-        return image;
-    }
-    public void dfs(int[][] image, int r, int c, int currColor, int color) {
-        
-		// return = break
-		
-		// Check out of bounds
-        if (r < 0 ) { return; } 
-        if (c < 0) { return; }
+  public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+    //Check if current color is equal with the current row/col
+    int currColor = image[sr][sc];
 
-		// check rows
-        if (r >= image.length) { return; }
-
-		// check check columns
-        if (c >= image[0].length) { return; }
-        if (image[r][c] != currColor) { return; }
-        
-		//the current change the color
-        image[r][c] = color;
-        dfs(image, r-1, c, currColor, color);
-        dfs(image, r, c-1, currColor, color);
-        dfs(image, r+1, c, currColor, color);
-        dfs(image, r, c+1, currColor, color);
+    // base case
+    if (currColor != color) {
+      dfs(image, sr, sc, currColor, color);
     }
+    return image;
+  }
+
+  public void dfs(int[][] image, int r, int c, int currColor, int color) {
+    // ex. currColor = 1, Change color = 2
+
+    // Check out of bounds
+    if (r < 0) { return; }
+    if (c < 0) { return; }
+    if (r >= image.length) { return; }
+    if (c >=image[0].length) { return; }
+
+    // Check current color
+    if (image[r][c] != currColor) { return; }
+
+    image[r][c] = color;
+    dfs(image, r-1, c, currColor, color);
+    dfs(image, r, c-1, currColor, color);
+    dfs(image, r+1, c, currColor, color);
+    dfs(image, r, c+1, currColor, color);
+  }
 }
-
-
