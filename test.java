7296d813d065a2e9f301ec982fe151fab9bdb366
zachuri <punsalang.zachary@gmail.com>
2023-02-13 06:51:07 -0800

    11: added solution

--- test.java
+++ test.java
@@ -1,33 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-		// Check if current color is equal with the current row/column
-        int currColor = image[sr][sc];
-		if (currColor != color) {
-            dfs(image, sr, sc, currColor, color);
-		}
-        return image;
-    }
-    public void dfs(int[][] image, int r, int c, int currColor, int color) {
-        
-		// return = break
-		
-		// Check out of bounds
-        if (r < 0 ) { return; } 
-        if (c < 0) { return; }
+  public boolean isBalanced(TreeNode root) {
+    return maxDepth != -1;    
+  }
 
-		// check rows
-        if (r >= image.length) { return; }
-
-		// check check columns
-        if (c >= image[0].length) { return; }
-        if (image[r][c] != currColor) { return; }
-        
-		//the current change the color
-        image[r][c] = color;
-        dfs(image, r-1, c, currColor, color);
-        dfs(image, r, c-1, currColor, color);
-        dfs(image, r+1, c, currColor, color);
-        dfs(image, r, c+1, currColor, color);
+  public int maxDepth(TreeNode root) {
+    
+    if (root == null) {
+      return 0;
     }
+
+    int l = maxDepth(root.left);
+    int r = maxDepth(root.left);
+
+    if (l == -1 || r == -1) {
+      return -1;
+    }
+
+    if (Math.abs(l - r) > 1) {
+      return -1;
+    }
+
+    return 1 + Math.max(l, r);
+
+  }
 }
-
