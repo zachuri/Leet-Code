a940f00bcd97320283126b0c4403a8ba053add9b
zachuri <punsalang.zachary@gmail.com>
2023-03-15 08:55:08 -0700

    28: 01 Matrix solution with comments

--- 28-01-Matrix.java
+++ 28-01-Matrix.java
@@ -1,35 +1,55 @@
 class Solution {
+  // MAX Possible distance
   private static final int MAX_DISTANCE = 10000;      
 
   public int[][] updateMatrix(int[][] mat) {
+    // init matrix rows anc cols
     int m = mat.length, n = mat[0].length;
 
+    // init distance matrix
     int[][] distance = new int[m][n];
 
-    for (int r = 0; r < m; r++) {
-      Arrays.fill(distance[r], MAX_DISTANCE);
+    // fill distance matrix with max_distance
+    for (int i = 0; i < m; ++i) {
+      Arrays.fill(distance[i], MAX_DISTANCE);
     }
 
-    for (int r = 0; r < m; r++) {
-      for (int c = 0; c < n; c++) {
+    // checks above and left cells
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
+        // set distance matrix to 0 if mat is 0
         if (mat[r][c] == 0) {
-          distance[r][c] = 0;
+          distance[r][c] = 0; 
           continue;
         }
 
-        if (r > 0) distance[r][c] = Math.min(distance[r][c], distance[r - 1][c] + 1);
-        if (c > 0) distance[r][c] = Math.min(distance[r][c], distance[r][c - 1] + 1);
+        // find min distance of above cells
+        if (r > 0) {
+          distance[r][c] = Math.min(distance[r][c], distance[r - 1][c] + 1);
+        }
+
+        // find min distance of left cells
+        if (c > 0) {
+          distance[r][c] = Math.min(distance[r][c], distance[r][c - 1] + 1);
+        }
       }
     }
 
-    for (int r = m - 1; r >= 0; r--) {
-      for (int c = n - 1; c >= 0; c--) {
-        if (r != m - 1) distance[r][c] = Math.min(distance[r][c], distance[r + 1][c] + 1);
-        if (c != n - 1) distance[r][c] = Math.min(distance[r][c], distance[r][c + 1] + 1);
+    // checks below and right cells
+    for (int r = m - 1; r >= 0; --r) {
+      for (int c = n - 1; c >= 0; --c) {
+        // find min distance of below cells
+        if (r < m - 1) {
+          distance[r][c] = Math.min(distance[r][c], distance[r + 1][c] + 1);
+        }
+
+        // find min distance of right cells
+        if (c < n - 1) {
+          distance[r][c] = Math.min(distance[r][c], distance[r][c + 1] + 1);
+        }
       }
     }
 
     return distance;
-
   }
 }
